4c4d7f0e68b4bd764ebd1ef06fae7dc40f4f3dc9
JRSWebPage <jrspromoters@gmail.com>
2025-02-17 20:14:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,9 @@ def api_prediction():
     result = advanced_prediction()
     return jsonify(result)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000
+    app.run(host='0.0.0.0', port=port)
+
